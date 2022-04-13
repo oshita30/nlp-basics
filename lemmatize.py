17c3f68068fa46009f816516005a9f362e282587
Oshita <84271127+oshita30@users.noreply.github.com>
2022-04-14 03:12:29 +0530

    Create lemmatize.py

---
+++ lemmatize.py
@@ -0,0 +1,27 @@
+def lemmatize(token):
+  '''
+  to lemmatize the token based on pos_tag
+  
+  token: string word
+  return type: lemmatized string word
+  '''
+  
+  ps = PorterStemmer()
+  tag = nltk.pos_tag([token])[0][1]
+  lem_token = ''
+  if tag[0] == 'V':
+    lem_token = lemmatizer.lemmatize(token,pos='v')
+  elif tag[0] == 'J':
+    lem_token = lemmatizer.lemmatize(token,pos='a')
+  elif tag[0] == 'R':
+    try:
+      lem_token = wn.synset(token+'.r.2').lemmas()[0].pertainyms()[0].name()
+    except:
+      lem_token = wn.synset(token+'.r.1').lemmas()[0].pertainyms()[0].name()
+  else:
+    lem_token = lemmatizer.lemmatize(token,pos='n')
+  
+  if lem_token == token:
+    lem_token = ps.stem(token)
+  
+  return lem_token
